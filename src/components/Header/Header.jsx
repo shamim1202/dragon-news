b7c7e246daf869a70fa610baa2d6dc0ffba09a6a
shamim1202 <mehedi.farzu@gmail.com>
2025-10-18 00:46:40 +0600

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import { format } from "date-fns";
 import logo from "../../assets/logo.png";
 
 const Header = () => {
@@ -5,7 +6,12 @@ const Header = () => {
     <div>
       <div className="flex flex-col items-center justify-center gap-3">
         <img className="w-sm" src={logo} alt="" srcset="" />
-        <p className="text-[#706f6f]">Journalism Without Fear or Favour</p>
+        <p className="text-info">Journalism Without Fear or Favour</p>
+        <p className="font-semibold text-info">
+          <span className="text-accent">{format(new Date(), "EEEE")},</span>
+          {" "}
+          {format(new Date(), "MMMM dd, uuuu")}
+        </p>
       </div>
     </div>
   );
