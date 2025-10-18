8dbe1daba6763c03a6581970b0c51344744ffd0f
shamim1202 <mehedi.farzu@gmail.com>
2025-10-18 23:51:19 +0600

    fetch all news category wise

--- src/pages/CategoryNews/CategoryNews.jsx
+++ src/pages/CategoryNews/CategoryNews.jsx
@@ -1,12 +1,32 @@
-import { useParams } from "react-router";
+import { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router";
 
 const CategoryNews = () => {
-    const { id } = useParams()
-    return (
-        <div>
-            Category - {id} News
-        </div>
-    );
+  const { id } = useParams();
+  const newsData = useLoaderData();
+  const [categoryNews, setCategoryNews] = useState([]);
+
+  useEffect(() => {
+    if (id == "0") {
+      setCategoryNews(newsData);
+      return;
+    } else if (id == "1") {
+      const filteredNews = newsData.filter(
+        (news) => news.others.is_today_pick == true
+      );
+      setCategoryNews(filteredNews);
+      return;
+    } else {
+      const filteredNews = newsData.filter((news) => news.category_id == id);
+      setCategoryNews(filteredNews);
+    }
+  }, [newsData, id]);
+
+  return (
+    <div>
+      Category - {id} News {categoryNews.length}
+    </div>
+  );
 };
 
 export default CategoryNews;
